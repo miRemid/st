a0ecdd0340411d6cb4f820bffc2ce701d7b0126c
kamir <miosuki241@outlook.com>
2020-04-06 23:11:23 +0800

    更改翻页

--- config.h
+++ config.h
@@ -187,6 +187,10 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
+	{ ShiftMask,            XK_Up,     		kscrollup,      {.i =  1} },
+	{ ShiftMask,            XK_Down,   		kscrolldown,    {.i =  1} },
+	{ ShiftMask|ControlMask,XK_Up,     		kscrollup,      {.i = -1} },
+	{ ShiftMask|ControlMask,XK_Down,   		kscrolldown,    {.i = -1} },
 };
 
 /*
